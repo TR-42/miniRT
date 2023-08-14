fabd8d86e44bdabdf3b5663a5aeeed7b23aaf3ff
kfujita <kfujita@student.42tokyo.jp>
2023-08-14 17:55:55 +0900

    色付きの鏡面反射を実装?

--- srcs/main.c
+++ srcs/main.c
@@ -25,25 +25,36 @@
 
 #define CANVAS_HEIGHT 480
 #define CANVAS_WIDTH 640
+#define RAY_CHK_MAX 16
 
 static t_objs	g_obj_arr[16];
 
 static t_rgb	_ray_to_rgb(t_ray ray)
 {
 	t_hit	hit;
+	t_rgb	color;
 	bool	does_hit;
+	size_t	ray_chk_cnt;
 
 	hit = (t_hit){0};
-	does_hit = ray_hit_any(&ray, g_obj_arr, 2, &hit);
-	if (does_hit)
+	color = (t_rgb){0};
+	does_hit = true;
+	ray_chk_cnt = 0;
+	while (++ray_chk_cnt <= RAY_CHK_MAX)
 	{
-		return ((t_rgb){
-			.r = (int)fabs((hit.normal.x + 1) * 127.99),
-			.g = (int)fabs((hit.normal.y + 1) * 127.99),
-			.b = (int)fabs((hit.normal.z + 1) * 127.99),
-		});
+		does_hit = ray_hit_any(&ray, g_obj_arr, 6, &hit);
+		if (!does_hit)
+			break ;
+		ray.origin = hit.at;
+		ray.direction = vec3_sub(ray.direction, vec3_mul(hit.normal,
+					2 * vec3_dot(ray.direction, hit.normal)));
+		color = (t_rgb){
+			.r = color.r * 0.5 + hit.obj->sphere.color.r * 0.5,
+			.g = color.g * 0.5 + hit.obj->sphere.color.g * 0.5,
+			.b = color.b * 0.5 + hit.obj->sphere.color.b * 0.5,
+		};
 	}
-	return ((t_rgb){0});
+	return (color);
 }
 
 static void	_set_gradient(
@@ -57,9 +68,9 @@ static void	_set_gradient(
 
 	iy = 0;
 	cam = (t_cam){
-		.point = vec3_(0, 0, 0),
-		.orientation = vec3_normalize(vec3_(0, 0, 1)),
-		.fov = 60,
+		.point = vec3_(0, 2, 5),
+		.orientation = vec3_normalize(vec3_(0, 3, 10)),
+		.fov = 50,
 	};
 	while (iy < CANVAS_HEIGHT)
 	{
@@ -90,6 +101,10 @@ int	main(
 		return (perr_retint("canvas_init", 1));
 	g_obj_arr[0] = sphere_init(vec3_(0, 0, -2), 0.6, (t_rgb){255, 0, 0});
 	g_obj_arr[1] = sphere_init(vec3_(0, -100, 0), 99, (t_rgb){0, 255, 0});
+	g_obj_arr[2] = sphere_init(vec3_(1, -0.7, -2), 0.4, (t_rgb){255, 0, 255});
+	g_obj_arr[3] = sphere_init(vec3_(2, 0, -3), 0.4, (t_rgb){255, 255, 0});
+	g_obj_arr[4] = sphere_init(vec3_(-1, -0.2, -2), 0.4, (t_rgb){0, 0, 255});
+	g_obj_arr[5] = sphere_init(vec3_(-2, -0.6, -3), 0.4, (t_rgb){0, 255, 255});
 	_set_gradient(&canvas);
 	tmp = NULL;
 	len = canvas_to_png(&canvas, &tmp);
