1c2d94a973b768249f12491bf2d101b531297b31
kfujita <kfujita@student.42tokyo.jp>
2023-08-21 23:43:22 +0900

    amb lightの適用を別関数化

--- srcs/ray/to_rgb.c
+++ srcs/ray/to_rgb.c
@@ -13,6 +13,28 @@
 #include <scene.h>
 #include <utils.h>
 
+static t_rgb	_apply_amb_light(
+	const t_scene *scene,
+	const t_ray *ray,
+	const t_hit *hit
+)
+{
+	t_vec3	tmp;
+	double	ref_rate;
+
+	tmp = vec3_sub(
+			ray->direction,
+			vec3_mul(hit->normal, 2 * vec3_dot(ray->direction, hit->normal))
+			);
+	ref_rate = scene->amb_light.ratio;
+	ref_rate *= vec3_dot(hit->normal, vec3_normalize(tmp));
+	return (brend_rgb(
+			scene->amb_light.color,
+			hit->obj->sphere.color,
+			ref_rate)
+	);
+}
+
 __attribute__((nonnull))
 t_rgb	ray_to_rgb(
 	t_ray ray,
@@ -22,17 +44,11 @@ t_rgb	ray_to_rgb(
 	t_hit	hit;
 	t_rgb	color;
 	t_objs	*objs;
-	double	ref_rate;
 
 	objs = (t_objs *)(scene->objs.p);
 	hit = (t_hit){0};
 	if (!ray_hit_any(&ray, objs, scene->objs.len, &hit))
 		return ((t_rgb){0});
-	color = scene->amb_light.color;
-	ref_rate = scene->amb_light.ratio;
-	ray.origin = hit.at;
-	ray.direction = vec3_normalize(vec3_sub(ray.direction,
-				vec3_mul(hit.normal, 2 * vec3_dot(ray.direction, hit.normal))));
-	ref_rate = vec3_dot(hit.normal, ray.direction) * ref_rate;
-	return (brend_rgb(color, hit.obj->sphere.color, ref_rate));
+	color = _apply_amb_light(scene, &ray, &hit);
+	return (color);
 }
