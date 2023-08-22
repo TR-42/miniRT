f2b370f0614c181254da68da91195fc9a67a6c4c
kfujita <kfujita@student.42tokyo.jp>
2023-08-22 23:21:00 +0900

    鏡面反射光を実装

--- srcs/ray/to_rgb.c
+++ srcs/ray/to_rgb.c
@@ -10,24 +10,66 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <math.h>
+
 #include <scene.h>
 #include <utils.h>
 
+// RRR: Reflection Rate Ratio
+#define RRR_AMB 1
+#define RRR_DIRECT 69
+#define RRR_MIRROR 30
+#define MIRROR_REF_ALPHA 8
+
 static double	_get_amb_light_ref_rate(
-	const t_scene *scene,
-	const t_ray *ray,
-	const t_hit *hit
+	const t_scene *scene
 )
 {
-	t_vec3	tmp;
 	double	ref_rate;
 
-	tmp = vec3_reflect(
-			vec3_mul(ray->direction, -1),
-			hit->normal
-			);
 	ref_rate = scene->amb_light.ratio;
-	return (ref_rate * vec3_dot(hit->normal, vec3_normalize(tmp)));
+	ref_rate *= (double)RRR_AMB / (RRR_AMB + RRR_DIRECT + RRR_MIRROR);
+	return (ref_rate);
+}
+
+static double	_get_direct_ref_rate(
+	const t_scene *scene,
+	const t_hit *hit,
+	const t_vec3 *hit_to_light
+)
+{
+	double	dot_n_l;
+	double	ref_rate;
+
+	dot_n_l = vec3_dot(hit->normal, *hit_to_light);
+	if (dot_n_l <= 0)
+		return (0);
+	ref_rate = scene->light.brightness;
+	ref_rate *= (double)RRR_DIRECT / (RRR_AMB + RRR_DIRECT + RRR_MIRROR);
+	ref_rate *= dot_n_l;
+	return (ref_rate);
+}
+
+static double	_get_mirror_ref_rate(
+	const t_scene *scene,
+	const t_ray *ray,
+	const t_hit *hit,
+	const t_vec3 *hit_to_light
+)
+{
+	double	ref_rate;
+	double	dot_v_r;
+
+	dot_v_r = vec3_dot(
+			vec3_mul(ray->direction, -1),
+			vec3_normalize(vec3_reflect(*hit_to_light, hit->normal))
+			);
+	if (dot_v_r <= 0)
+		return (0);
+	ref_rate = scene->light.brightness;
+	ref_rate *= (double)RRR_MIRROR / (RRR_AMB + RRR_DIRECT + RRR_MIRROR);
+	ref_rate *= pow(dot_v_r, MIRROR_REF_ALPHA);
+	return (ref_rate);
 }
 
 __attribute__((nonnull))
@@ -38,12 +80,17 @@ t_rgb	ray_to_rgb(
 {
 	t_hit	hit;
 	t_objs	*objs;
+	t_vec3	hit_to_light;
 	double	ref_rate;
 
 	objs = (t_objs *)(scene->objs.p);
 	hit = (t_hit){0};
 	if (!ray_hit_any(&ray, objs, scene->objs.len, &hit))
 		return ((t_rgb){0});
-	ref_rate = _get_amb_light_ref_rate(scene, &ray, &hit);
+	hit_to_light = vec3_normalize(vec3_sub(scene->light.point, hit.at));
+	ref_rate = _get_direct_ref_rate(scene, &hit, &hit_to_light);
+	if (ref_rate != 0)
+		ref_rate += _get_mirror_ref_rate(scene, &ray, &hit, &hit_to_light);
+	ref_rate += _get_amb_light_ref_rate(scene);
 	return (brend_rgb(scene->amb_light.color, hit.obj->sphere.color, ref_rate));
 }
